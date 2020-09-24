df26d5d2b559437d54e3067a9da1218b1012fefb
Lev Lymarenko <lymarenko.lev@gmail.com>
2020-09-24 17:00:52 +0300

    cridentials in separate file

--- config.py
+++ config.py
@@ -1,13 +1,5 @@
 import os
-
-
-credentials = {
-    'lev':{
-        'api_id': 1,
-        'api_hash': '1',
-        'session': 'account0.session'
-    }
-}
+from cridentials import credentials
 
 CHECK_INTERVAL = int(os.environ.get('CHECK_INTERVAL', ''))
 
---
+++ cridentials.py
@@ -0,0 +1,7 @@
+credentials = {
+    'lev':{
+        'api_id': 1,
+        'api_hash': '1',
+        'session': 'account0.session'
+    }
+}

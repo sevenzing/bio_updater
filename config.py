c9f6280590fe6f46cb5e5e10a65e95933d33521d
Lev Lymarenko <lymarenko.lev@gmail.com>
2020-09-24 16:59:16 +0300

    config example

--- config.py
+++ config.py
@@ -2,6 +2,11 @@ import os
 
 
 credentials = {
+    'lev':{
+        'api_id': 1,
+        'api_hash': '1',
+        'session': 'account0.session'
+    }
 }
 
 CHECK_INTERVAL = int(os.environ.get('CHECK_INTERVAL', ''))

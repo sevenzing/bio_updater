06843ab15005c39fba90334f32e2475c11a9cdc3
Lev Lymarenko <lymarenko.lev@gmail.com>
2020-09-24 18:49:44 +0300

    avoid flood error

--- client.py
+++ client.py
@@ -1,5 +1,6 @@
 from telethon import TelegramClient, events, sync
 from telethon.tl.types import Dialog
+from telethon.errors.rpcerrorlist import FloodWaitError
 from telethon.tl.functions.messages import ImportChatInviteRequest
 from telethon.tl.functions.channels import JoinChannelRequest
 from telethon.tl.functions.account import UpdateProfileRequest
@@ -9,7 +10,8 @@ import logging
 
 from config import BIO_MESSAGE
 from time_tools import get_current_time_in_words, get_now
-
+from asyncio import sleep
+from datetime import timedelta
 
 class Client:
     def __init__(self, session, api_id, api_hash, try_logging_in=False):
@@ -18,14 +20,23 @@ class Client:
         self.api_hash =  api_hash 
         self.account = TelegramClient(session, api_id, api_hash)
         self.account.connect()
+        self.lastupdate = get_now() - timedelta(minutes=1)
         if try_logging_in:
             self.account.start()
 
     async def edit_bio(self, new_bio):
-        logging.info(f"Change bio to {new_bio}")
-        await self.account(UpdateProfileRequest(
-            about=new_bio,
-        ))
+        logging.debug('check for changing bio')
+        # 1 minute
+        try:
+            if get_now().minute != self.lastupdate.minute:
+                logging.info(f"Change bio to {new_bio}")
+                await self.account(UpdateProfileRequest(
+                    about=new_bio,
+                ))
+                self.lastupdate = get_now()
+        except FloodWaitError as e:
+            logging.warning(f"Got flood message: {e}. sleep for {e.seconds}")
+            await sleep(e.seconds)
 
     async def set_time_to_bio(self):
         await self.edit_bio(BIO_MESSAGE % get_current_time_in_words())

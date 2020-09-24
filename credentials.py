893ce00d9516910fc32ae7da8d9b302a38960390
Lev Lymarenko <lymarenko.lev@gmail.com>
2020-09-24 17:15:26 +0300

    file rename

---
+++ credentials.py
@@ -0,0 +1,6 @@
+credentials = {    
+    'api_id': 922991,
+    'api_hash': '1627a398cf9db5c7e10f0bb57a6538bd',
+    'session': 'account0.session'
+    
+}
--- cridentials.py
+++
@@ -1,7 +0,0 @@
-credentials = {
-    'lev':{
-        'api_id': 1,
-        'api_hash': '1',
-        'session': 'account0.session'
-    }
-}

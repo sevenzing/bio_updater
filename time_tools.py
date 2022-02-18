68a47a61cbbe4efb43f99a4b033a1f93e196e9d5
Lev Lymarenko <lymarenko.lev@gmail.com>
2022-02-18 18:05:09 +0300

    = time tools

--- time_tools.py
+++ time_tools.py
@@ -1,9 +1,24 @@
-import datetime
-from datetime import timedelta
-from random import randrange, randint, choice
-from russian import number_to_russian, hour_in_nominative, hour_in_genitive, minute_in_genitive, minute_in_nominative
 from config import TIME_ZONE
+from datetime import timedelta
+from random import randrange, randint
+import random
+import dataclasses
+import datetime
 import pytz
+import typing
+
+from russian import hour_in_nominative, hour_in_genitive, minute_in_genitive, minute_in_nominative
+
+@dataclasses.dataclass
+class TranslateOption:
+    """
+    Possible option to represent datetime and weight to pick this datetime
+    """
+    value: str
+    weight: float = dataclasses.field(default=1, compare=False)
+
+
+T = TranslateOption
 
 def random_date(start, end):
     """
@@ -16,11 +31,10 @@ def random_date(start, end):
     return start + timedelta(seconds=random_second)
 
 
-def percent_chance(percent):
-    return randint(0, 99) in range(percent)
-
-
-def translate_time(date: datetime.datetime):
+def _generate_translate_options(date: datetime.datetime):
+    """
+    Generate possible options to represent datetime.
+    """
     hour, minute = date.hour, date.minute
 
     hour_word = hour_in_nominative(hour)
@@ -29,34 +43,46 @@ def translate_time(date: datetime.datetime):
     single_minute_word = minute_in_nominative(minute, only_number=True)
 
     if minute == 0:
-        return f"ровно {hour_in_nominative(hour)}"
+        yield T(f"ровно {hour_word}", 20)
+
     elif minute == 30 and hour % 12 != 0:
-        return f"пол {hour_in_genitive(hour % 12)}"
+        yield T(f"пол {hour_in_genitive(hour % 12 + 1)}", 20)
 
     elif hour == 0:
         if minute == 0:
-            return "ровно полуночь"
+            yield T("ровно полночь", 20)
         else:
-            return f"{minute_word} после полуночи"
+            yield T(f"{minute_word} после полуночи", 10)
     
     elif hour == 12:
         if minute == 0:
-            return "ровно полудень"
+            yield T("ровно полдень", 20)
         else:
-            return f"{minute_word} после полудня"
-
-    elif  minute >= 45 and percent_chance((45 * minute + 144 * hour)%100):
-        # 'Без' mode
-        if minute == 45:
-            return f"без {choice(['пятнадцати', 'четверти'])} {hour_in_nominative(hour + 1, only_number=True)}"
-        return f"без {minute_in_genitive(60 - minute)} минут {hour_in_nominative(hour + 1, only_number=True)}"
-    elif 1 <= minute <= 9 and percent_chance(80):
-        return f"{single_hour_word} ноль {single_minute_word}"
+            yield T(f"{minute_word} после полудня", 10)
     else:
-        if minute % 10 == 0 and percent_chance(80):
-            return ' '.join([single_hour_word, single_minute_word])   
-        else:
-            return ' '.join([hour_word, minute_word]) 
+        # 'Без' mode
+        if minute >= 45:
+            if minute == 45:
+                yield T(f"без четверти {hour_in_nominative(hour + 1, only_number=True)}", 10)
+            yield T(f"без {minute_in_genitive(60 - minute)} минут {hour_in_nominative(hour + 1, only_number=True)}", 5)
+        
+        if 1 <= minute <= 9:
+            yield T(f"{single_hour_word} ноль {single_minute_word}", 6)
+
+        if minute % 10 == 0:
+            yield T(' '.join([single_hour_word, single_minute_word]), 8)
+
+        yield T(' '.join([hour_word, minute_word]), 1)
+
+
+def translate_time(date: datetime.datetime) -> str:
+    options = list(_generate_translate_options(date))
+    return random.choices(
+        population=[o.value for o in options],
+        weights=[o.weight for o in options],
+        k=1,
+    )[0]
+
 
 def get_now() -> datetime.datetime:
     return  datetime.datetime.now(

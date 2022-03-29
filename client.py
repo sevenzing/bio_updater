b78126f005abaabdfc3b5301340857297d17e5b8
Lymarenko Lev <lymarenko.lev@gmail.com>
2022-03-29 09:41:17 +0300

    ! bioistoolong error

--- client.py
+++ client.py
@@ -1,18 +1,16 @@
-from telethon import TelegramClient, events, sync
-from telethon.tl.types import Dialog
-from telethon.errors.rpcerrorlist import FloodWaitError
-from telethon.tl.functions.messages import ImportChatInviteRequest
-from telethon.tl.functions.channels import JoinChannelRequest
-from telethon.tl.functions.account import UpdateProfileRequest
-from typing import List, Dict
-import re
 import logging
+import telethon
 
-from config import BIO_MESSAGES, TIME_LEFT_MESSAGE
-from random import choice 
-from time_tools import get_current_time_in_words, get_now, get_passed_time_in_words
 from asyncio import sleep
 from datetime import timedelta
+from random import choice 
+from telethon import TelegramClient
+from telethon.errors.rpcerrorlist import FloodWaitError
+from telethon.tl.functions.account import UpdateProfileRequest
+
+from config import BIO_MESSAGES, TIME_LEFT_MESSAGE
+from time_tools import get_current_time_in_words, get_now, get_passed_time_in_words
+
 
 class Client:
     def __init__(self, session, api_id, api_hash, try_logging_in=False):
@@ -43,7 +41,10 @@ class Client:
         await self.edit_bio(choice(BIO_MESSAGES) % get_current_time_in_words())
     
     async def set_time_left_to_bio(self):
-        await self.edit_bio(TIME_LEFT_MESSAGE % get_passed_time_in_words())
+        try:
+            await self.edit_bio(TIME_LEFT_MESSAGE % get_passed_time_in_words())
+        except telethon.errors.rpcerrorlist.AboutTooLongError:
+            pass
 
     def __str__(self):
         me = self.account.get_me()
